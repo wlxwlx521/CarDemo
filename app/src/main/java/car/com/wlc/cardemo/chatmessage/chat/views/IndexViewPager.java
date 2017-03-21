37f8dfa6024d482e6e8c3c28c97a7dc8d0f916c8
liyunfu <859524441@qq.com>
2017-03-21 10:05:50 +0800

    indexViewPager

---
+++ app/src/main/java/car/com/wlc/cardemo/chatmessage/chat/views/IndexViewPager.java
@@ -0,0 +1,64 @@
+package car.com.wlc.cardemo.chatmessage.chat.views;
+
+import android.content.Context;
+import android.support.v4.view.ViewPager;
+import android.util.AttributeSet;
+import android.view.MotionEvent;
+
+/**
+ * Created by Administrator on 2017/3/21.
+ */
+
+public class IndexViewPager extends ViewPager {
+    boolean isCanScroll;
+
+    public IndexViewPager(Context context) {
+        super(context);
+    }
+
+    public IndexViewPager(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public void setScanScroll(boolean isCanScroll) {
+        this.isCanScroll = isCanScroll;
+    }
+
+    @Override
+    public void scrollTo(int x, int y) {
+        super.scrollTo(x, y);
+    }
+
+    @Override
+    public boolean onTouchEvent(MotionEvent arg0) {
+        // TODO Auto-generated method stub
+        if (isCanScroll) {
+            return super.onTouchEvent(arg0);
+        } else {
+            return false;
+        }
+    }
+
+    @Override
+    public void setCurrentItem(int item, boolean smoothScroll) {
+        // TODO Auto-generated method stub
+        super.setCurrentItem(item, smoothScroll);
+    }
+
+    @Override
+    public void setCurrentItem(int item) {
+        // TODO Auto-generated method stub
+        super.setCurrentItem(item);
+    }
+
+    @Override
+    public boolean onInterceptTouchEvent(MotionEvent arg0) {
+        // TODO Auto-generated method stub
+        if (isCanScroll) {
+            return super.onInterceptTouchEvent(arg0);
+        } else {
+            return false;
+        }
+
+    }
+}
